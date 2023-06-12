973ceae0751d1fd2e87bf8e2ad6049de58fb24dd
HeroutRozilyo <heroot12@gmail.com>
2023-06-12 11:42:10 +0300

    todos

--- src/js/TodoItem.js
+++ src/js/TodoItem.js
@@ -3,7 +3,7 @@ import '../css/todos.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-<<<<<<< HEAD
+
 function TodoItem({ updateTodo, deleteTodo, todo }) {
   const [classItem, setClassItem] = useState('');
   const [title, setTitle] = useState(todo.title);
@@ -63,50 +63,4 @@ function TodoItem({ updateTodo, deleteTodo, todo }) {
   }  
 
 export default TodoItem;
-=======
-function TodoItem({updateTodo, todo}) {
-    const [classItem, setClassItem] = useState('');
-    const [title, setTitle] = useState(todo.title);
-    const titleRef = useRef(null);
 
-    const updateTodoComplete = async () => {
-        await updateTodo(todo, {complete: !todo.complete});
-    }
-    
-    const editItem = () => {
-      setClassItem(' edit');
-      titleRef.current.focus();
-      titleRef.current.setSelectionRange(0, title.length);
-    }
-
-    const updateTodoTitle = async () => {
-      await updateTodo(todo, {title: title});
-      setClassItem('');
-    }
-    
-    const itemKeyPress = (e) => {
-      if (e.keyCode === 13) {
-        updateTodoTitle();
-      }
-    }
-
-    return (
-        <li className="todos-item">
-            <input
-              className="todos-checkbox"
-              type="checkbox"
-              checked={todo.complete}
-              onChange={() => updateTodoComplete()}
-            />
-            <span className={"title-item" + classItem}
-                onClick={editItem}
-                onBlur={() => updateTodoTitle()}
-                onKeyUp={itemKeyPress}>
-            <span className="todos-text">{todo.title}</span>
-            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} ref={titleRef} />
-            </span>
-        </li>
-    );
-}
-export default TodoItem;
->>>>>>> b733fd7b3686990d8087df1de093c4e4dfd46f91
