93bb0717630700b4a464af11c588f911fb36fa6e
HeroutRozilyo <heroot12@gmail.com>
2023-06-16 16:50:45 +0300

    deletePhotos

--- src/js/AlbumsItem.js
+++ src/js/AlbumsItem.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import "../css/albumItem.css";
 
 function AlbumsItem() {
@@ -11,8 +13,8 @@ function AlbumsItem() {
   const navigate = useNavigate();
 
   const user = JSON.parse(localStorage.getItem("user"));
-  if(user.username !== username) {
-    navigate(`users/${user.username}/albums/${id}/photos`)
+  if (user.username !== username) {
+    navigate(`users/${user.username}/albums/${id}/photos`);
   }
 
   useEffect(() => {
@@ -38,6 +40,28 @@ function AlbumsItem() {
     navigate(`/users/${username}/albums`);
   };
 
+  const handleDeletePhoto = (photoId) => {
+    // Logic to delete the photo with the given photoId
+    fetch(`http://localhost:3001/api/photos/${photoId}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log(`Successfully deleted photo with ID: ${photoId}`);
+          setPhotos(photos.filter((photo) => photo.id !== photoId));
+          // Perform any additional actions after successful deletion
+        } else {
+          console.log(`Failed to delete photo with ID: ${photoId}`);
+          // Handle the error case if deletion fails
+        }
+      })
+      .catch((error) => {
+        console.log(`Error deleting photo with ID: ${photoId}`, error);
+        // Handle any network or other errors
+      });
+  };
+  
+
   return (
     <div className="photo-list-container">
       <button className="view-comments-button" onClick={handleBackClick}>
@@ -46,12 +70,18 @@ function AlbumsItem() {
       {photos.length > 0 ? (
         <div className="photo-list">
           {photos.slice(start, end).map((photo) => (
-            <img
-              key={photo.id}
-              src={photo.thumbnailUrl}
-              alt={photo.title}
-              className="photo-item"
-            />
+            <div key={photo.id} className="photo-item-container">
+              <img
+                src={photo.thumbnailUrl}
+                alt={photo.title}
+                className="photo-item"
+              />
+              <FontAwesomeIcon
+                icon={faTrash}
+                className="delete-icon"
+                onClick={() => handleDeletePhoto(photo.id)}
+              />
+            </div>
           ))}
         </div>
       ) : (
